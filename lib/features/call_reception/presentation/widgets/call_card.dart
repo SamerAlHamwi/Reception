15d7dc6c7377750be8485a80c41c6f258840b652
Hanan khateb <hanan.khateb.123321@gmail.com>
2023-03-06 14:24:03 +0200

    change status color

--- lib/features/call_reception/presentation/widgets/call_card.dart
+++ lib/features/call_reception/presentation/widgets/call_card.dart
@@ -119,7 +119,7 @@ class CallCard extends StatelessWidget {
     } else if (status == "Canceled".tr()) {
       return Colors.redAccent;
     } else if (status == "Active".tr()) {
-      return Colors.green;
+      return Colors.deepOrange;
     }
   }
 
